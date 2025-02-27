c46bc0dfb8a624f4264335124359ec1c5d1f690d
Jørgen Vatle <jorgen@wcaservices.net>
2025-02-27 04:15:29 +0200

    Mark vitest as external

--- npm-packages/meteor-vite/tsup.config.ts
+++ npm-packages/meteor-vite/tsup.config.ts
@@ -31,6 +31,7 @@ export default defineConfig([
             }
         },
         noExternal: ['meteor'],
+        external: ['vitest'],
         esbuildPlugins: [
             EsbuildPluginMeteorStubs,
         ]
