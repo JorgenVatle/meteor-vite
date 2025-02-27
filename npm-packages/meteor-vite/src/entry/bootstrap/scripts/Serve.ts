19b49f06b5c415dc93297df7c6f797df00d5c571
Jørgen Vatle <jorgen@wcaservices.net>
2025-02-27 05:39:55 +0200

    Add test bootstrapper script

--- npm-packages/meteor-vite/src/entry/bootstrap/scripts/Serve.ts
+++ npm-packages/meteor-vite/src/entry/bootstrap/scripts/Serve.ts
@@ -1,3 +1,4 @@
+import { createVitest } from 'vitest/node';
 import { ViteDevelopmentBoilerplate } from '../boilerplate/Development';
 import { resolveMeteorViteConfig } from '../lib/Config';
 
@@ -15,4 +16,25 @@ export async function prepareDevServerBoilerplate() {
         needsReactPreamble,
         baseUrl: config.base,
     });
+}
+
+export async function runTests() {
+    const { config, modules,  } = await resolveMeteorViteConfig({
+        mode: 'development',
+    }, 'serve');
+    
+    const vitest = await createVitest('test', {
+        config: config.configFile,
+        watch: false,
+    });
+    
+    // Todo: Create custom runner that will run tests within the current Meteor environment.
+    const results = await vitest.start();
+    console.log('Vitest result: ', results);
+    
+    if (vitest.state.getCountOfFailedTests()) {
+        throw new Error('Tests failed')
+    }
+    
+    
 }
