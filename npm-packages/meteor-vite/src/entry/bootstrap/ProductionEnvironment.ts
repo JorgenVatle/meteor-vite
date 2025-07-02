e5ddaa2bdf283ae28565f5e0d95031ff6f6bd6f8
Jørgen Vatle <Jorgen@WcAServices.net>
2025-07-03 01:02:58 +0300

    Add notice about debugging production build when serving 404s for Vite assets

--- npm-packages/meteor-vite/src/entry/bootstrap/ProductionEnvironment.ts
+++ npm-packages/meteor-vite/src/entry/bootstrap/ProductionEnvironment.ts
@@ -26,6 +26,12 @@ Meteor.startup(async () => {
         res.write('Vite asset not found');
         res.end();
         Logger.warn(`Served 404 for unknown Vite asset: ${req.originalUrl}`);
+        Logger.warnOnce({ id: 'vite-asset-not-found' }, [
+            'If you expected this asset to exist, please open an issue over on GitHub.',
+            `You can debug what's being included in your bundle by setting DEBUG='vite-bundler:*' when building for production`,
+            '',
+            'Do note that that this may happen if a client just loaded a cached version of your app as the browser will try to re-validate assets.',
+        ].join('\n'))
     });
     
     // Todo: Instead of serving assets with Meteor's built-in static file handler,
