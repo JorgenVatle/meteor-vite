fc78a454f840414e962d0d99ec1c71047a95cda3
Jørgen Vatle <Jorgen@WcAServices.net>
2025-07-04 20:38:47 +0300

    Use Vite mode instead of env for determining whether to launch the dev server

--- npm-packages/meteor-vite/src/entry/bootstrap/scripts/Serve.ts
+++ npm-packages/meteor-vite/src/entry/bootstrap/scripts/Serve.ts
@@ -3,7 +3,7 @@ import { ViteDevelopmentBoilerplate } from '../boilerplate/Development';
 import { resolveMeteorViteConfig } from '../lib/Config';
 
 export async function initializeViteDevServer() {
-    if (import.meta.env.PROD) {
+    if (import.meta.env.MODE !== 'development') {
         Logger.info('Prevented the Vite dev server from firing up, as you are already running a production build.');
         return;
     }
