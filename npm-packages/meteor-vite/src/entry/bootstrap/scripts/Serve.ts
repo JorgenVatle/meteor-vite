eb815c6ef8b9abce37f0b18d974700e227d37483
Jørgen Vatle <Jorgen@WcAServices.net>
2025-07-04 22:48:58 +0300

    Handle undefined import meta when initializing Vite dev server in non-production environments

--- npm-packages/meteor-vite/src/entry/bootstrap/scripts/Serve.ts
+++ npm-packages/meteor-vite/src/entry/bootstrap/scripts/Serve.ts
@@ -10,7 +10,7 @@ export async function initializeViteDevServer() {
      * production bundle as that is likely not desired behavior and won't work
      * anyway since dev dependencies aren't available.
      */
-    if (import.meta.env.MODE !== 'development') {
+    if (import.meta?.env?.MODE !== 'development') {
         Logger.info('Prevented the Vite dev server from firing up, as you are already running a production build.');
         return;
     }
