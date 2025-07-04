b81601187f2b93616f1f10452a5571c18b9bb1cb
Jørgen Vatle <Jorgen@WcAServices.net>
2025-07-04 20:27:50 +0300

    Prevent development environment from launching when running a production build

--- npm-packages/meteor-vite/src/entry/bootstrap/scripts/Serve.ts
+++ npm-packages/meteor-vite/src/entry/bootstrap/scripts/Serve.ts
@@ -1,7 +1,12 @@
+import Logger from '../../../utilities/Logger';
 import { ViteDevelopmentBoilerplate } from '../boilerplate/Development';
 import { resolveMeteorViteConfig } from '../lib/Config';
 
 export async function initializeViteDevServer() {
+    if (import.meta.env.PROD) {
+        Logger.info('Prevented the Vite dev server from firing up, as you are already running a production build.');
+        return;
+    }
     await import ('../DevelopmentEnvironment');
 }
 
