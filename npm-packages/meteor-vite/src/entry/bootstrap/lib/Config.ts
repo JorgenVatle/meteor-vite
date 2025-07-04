3830455590dbe47464718b199f19d276b6b71460
Jørgen Vatle <Jorgen@WcAServices.net>
2025-07-05 00:02:19 +0300

    Move project validation warning messages into project instance log emitter

--- npm-packages/meteor-vite/src/entry/bootstrap/lib/Config.ts
+++ npm-packages/meteor-vite/src/entry/bootstrap/lib/Config.ts
@@ -2,8 +2,6 @@ import FS from 'fs';
 import Path from 'path';
 import { createRunnableDevEnvironment, type InlineConfig, resolveConfig } from 'vite';
 import { MeteorViteError } from '../../../error/MeteorViteError';
-import { Colorize } from '../../../utilities';
-import Logger from '../../../utilities/Logger';
 import { meteorWorker } from '../../plugin/Meteor';
 import { type ProjectJson, type ResolvedMeteorViteConfig } from '../../plugin/Settings';
 import { clientMainModule, serverMainModule } from '../scripts/Setup';
@@ -22,30 +20,10 @@ export async function resolveMeteorViteConfig(
     const packageJson = parsePackageJson();
     process.chdir(projectRoot);
     
-    if (FS.existsSync(Path.join(projectRoot, '.meteorignore'))) {
-        Logger.warnOnce({ id: '.meteorignore' }, [
-            `Detected ${Colorize.fileType('.meteorignore')} file.`,
-            `Make sure that the paths within won't match any files within ${Colorize.filepath('./_vite-bundle')} as this`,
-            `could lead to certain assets not being available in production.`,
-            '',
-            `Anything outside of this directory you're free to ignore, you can even ignore source`,
-            `files as long as they are imported by your Vite entry module.\n\n`,
-        ].join('\n   '));
-    }
-    
-    const nonEsmConfigFile = FS.existsSync(Path.join(projectRoot, 'vite.config.ts')) || FS.existsSync(Path.join(projectRoot, 'vite.config.js'));
-    
-    if (packageJson.type !== 'module' && nonEsmConfigFile) {
-        Logger.warnOnce({ id: '.viteignore' }, [
-            `Vite config without .mjs or .mts extension detected.`,
-            'This will likely prevent Meteor from starting when trying to resolve your config.',
-            'Renaming vite.config.ts to vite.config.mts should resolve the issue this in most cases',
-            '',
-            'Setting "type": "module" in your package.json should fix this, but Meteor lacks good support for this',
-            'at the time of writing. The best workaround for using package.json "module" types is to symlink your',
-            '.meteor/local directory outside of your project root (e.g. ln -s /tmp/.meteor-local/my-app .meteor/local)\n\n',
-        ].join('\n   '))
-    }
+    /**
+     * Check for and warn users of potential project misconfigurations
+     */
+    Instance.emitWarningMessages(packageJson)
     
     /**
      * Only available within the context of the compiler plugin.
--- npm-packages/meteor-vite/src/entry/bootstrap/lib/Instance.ts
+++ npm-packages/meteor-vite/src/entry/bootstrap/lib/Instance.ts
@@ -1,9 +1,13 @@
+import FS from 'fs';
 import { Meteor } from 'meteor/meteor';
+import Path from 'path';
 import pc from 'picocolors';
 import { version as viteVersion } from 'vite';
+import { Colorize } from '../../../utilities';
 import { version } from '../../../utilities/Constants';
-import { createSimpleLogger } from '../../../utilities/Logger';
-import type { ResolvedMeteorViteConfig } from '../../plugin/Settings';
+import Logger, { createSimpleLogger } from '../../../utilities/Logger';
+import type { ProjectJson, ResolvedMeteorViteConfig } from '../../plugin/Settings';
+import { CurrentConfig } from './Config';
 
 const startTime = performance.now();
 // The global Meteor instance may not initially be defined within the plugin context during builds.
@@ -35,5 +39,33 @@ export default new class Instance {
             pc.cyan(`ready in ${Math.round(performance.now() - startTime).toLocaleString()}ms.`),
             '',
         ].join('\n    '));
+    }
+    
+    public emitWarningMessages(packageJson: ProjectJson) {
+        const { projectRoot} = CurrentConfig;
+        if (FS.existsSync(Path.join(projectRoot, '.meteorignore'))) {
+            Logger.warnOnce({ id: '.meteorignore' }, [
+                `Detected ${Colorize.fileType('.meteorignore')} file.`,
+                `Make sure that the paths within won't match any files within ${Colorize.filepath('./_vite-bundle')} as this`,
+                `could lead to certain assets not being available in production.`,
+                '',
+                `Anything outside of this directory you're free to ignore, you can even ignore source`,
+                `files as long as they are imported by your Vite entry module.\n\n`,
+            ].join('\n   '));
+        }
+        
+        const nonEsmConfigFile = FS.existsSync(Path.join(projectRoot, 'vite.config.ts')) || FS.existsSync(Path.join(projectRoot, 'vite.config.js'));
+        
+        if (packageJson.type !== 'module' && nonEsmConfigFile) {
+            Logger.warnOnce({ id: '.viteignore' }, [
+                `Vite config without .mjs or .mts extension detected.`,
+                'This will likely prevent Meteor from starting when trying to resolve your config.',
+                'Renaming vite.config.ts to vite.config.mts should resolve the issue this in most cases',
+                '',
+                'Setting "type": "module" in your package.json should fix this, but Meteor lacks good support for this',
+                'at the time of writing. The best workaround for using package.json "module" types is to symlink your',
+                '.meteor/local directory outside of your project root (e.g. ln -s /tmp/.meteor-local/my-app .meteor/local)\n\n',
+            ].join('\n   '))
+        }
     }
 }
