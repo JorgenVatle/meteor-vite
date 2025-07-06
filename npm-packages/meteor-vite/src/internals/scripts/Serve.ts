ef0779a6a229465ad522b938439932e215dd9cb2
Jørgen Vatle <Jorgen@WcAServices.net>
2025-07-07 02:57:29 +0300

    Add missing vite/client reference types to dev server init script

--- npm-packages/meteor-vite/src/internals/scripts/Serve.ts
+++ npm-packages/meteor-vite/src/internals/scripts/Serve.ts
@@ -1,3 +1,4 @@
+/// <reference types="vite/client" />
 import { ViteDevelopmentBoilerplate } from '@/internals/boilerplate/Development';
 import { resolveMeteorViteConfig } from '@/internals/lib/resolveMeteorViteConfig';
 import Logger from '../../utilities/Logger';
