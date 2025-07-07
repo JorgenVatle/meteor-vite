65389453e86df72a21615b90bef1fe9eed211f94
Jørgen Vatle <Jorgen@WcAServices.net>
2025-07-07 11:32:50 +0300

    Move tsup build config for jorgenvatle:vite into packages/vite

--- packages/vite/tsup.config.ts
+++ packages/vite/tsup.config.ts
@@ -1,19 +1,18 @@
 import { defineConfig } from 'tsup';
-import { EsbuildPluginMeteorStubs, fixBuildPluginCjsImports } from './build/tsup-plugins';
+import { EsbuildPluginMeteorStubs, fixBuildPluginCjsImports } from '../../build/tsup-plugins';
 
-export default defineConfig(() => ({
+export default defineConfig({
     name: 'jorgenvatle:vite',
     entry: [
-        './packages/vite/src/entry/server-runtime.ts',
-        './packages/vite/src/entry/build-plugin.ts'
+        './src/entry/server-runtime.ts',
+        './src/entry/build-plugin.ts',
     ],
-    outDir: './packages/vite/dist',
     splitting: false,
     target: 'es2022',
     platform: 'node',
     keepNames: false,
     minify: false,
-    tsconfig: "tsconfig.build.json",
+    tsconfig: "./tsconfig.json",
     sourcemap: true,
     format: 'esm',
     esbuildPlugins: [
@@ -21,4 +20,4 @@ export default defineConfig(() => ({
         EsbuildPluginMeteorStubs,
     ],
     noExternal: ['meteor/isobuild', /meteor\//]
-}));
+});
