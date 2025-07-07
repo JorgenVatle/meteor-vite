fe6e6b82d124edc8c70daff0154db34976147ded
Jørgen Vatle <Jorgen@WcAServices.net>
2025-07-07 19:49:50 +0300

    Add debug logs to module runners

--- npm-packages/meteor-vite/src/internals/lib/ModuleRunner.ts
+++ npm-packages/meteor-vite/src/internals/lib/ModuleRunner.ts
@@ -4,12 +4,14 @@ export class ModuleRunner {
     constructor() {}
     
     public runScript<TName extends ScriptName>(script: TName): Promise<Awaited<ScriptResult<TName>>> {
+        console.debug(`Running script ${script} from meteor-vite/internals`);
         return import('@/internals/scripts/index').then((scripts: any): Awaited<ScriptResult<TName>> => {
             return scripts[script]();
         })
     }
     
     public import<T extends ImportPath>(module: T): Promise<AvailableImports[T]> {
+        console.debug(`Importing ${module} from meteor-vite`);
         return import((`meteor-vite/${module}`));
     }
     
