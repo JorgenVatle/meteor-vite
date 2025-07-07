7e980c247abe393bc020ca846a645771492eb8f5
Jørgen Vatle <Jorgen@WcAServices.net>
2025-07-07 07:56:28 +0300

    Shorten import paths

--- npm-packages/meteor-vite/test/AutoImportQueue.test.ts
+++ npm-packages/meteor-vite/test/AutoImportQueue.test.ts
@@ -1,6 +1,6 @@
+import AutoImportQueue, { wait } from '@/plugin/meteor/package/AutoImportQueue';
+import { RefreshNeeded } from '@/plugin/ViteLoadRequest';
 import { describe, expect, it } from 'vitest';
-import AutoImportQueue, { wait } from '../src/plugin/meteor/package/AutoImportQueue';
-import { RefreshNeeded } from '../src/plugin/ViteLoadRequest';
 import { AutoImportMock } from './__mocks';
 
 describe('Package auto-imports', async () => {
