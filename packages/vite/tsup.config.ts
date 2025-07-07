ab84877e41c55be77e0dbf3d370c42f29028f874
Jørgen Vatle <Jorgen@WcAServices.net>
2025-07-07 12:08:02 +0300

    Use shared build config utility

--- packages/vite/tsup.config.ts
+++ packages/vite/tsup.config.ts
@@ -1,23 +1,18 @@
-import { defineConfig } from 'tsup';
-import { EsbuildPluginMeteorStubs, fixBuildPluginCjsImports } from '../../build/tsup-plugins';
+import { defineBuildConfig } from '../../build/defineBuildConfig';
+import { fixBuildPluginCjsImports } from '../../build/tsup-plugins';
 
-export default defineConfig({
+export default defineBuildConfig({
     name: 'jorgenvatle:vite',
     entry: [
         './src/entry/server-runtime.ts',
         './src/entry/build-plugin.ts',
     ],
     splitting: false,
-    target: 'es2022',
     platform: 'node',
-    keepNames: false,
-    minify: false,
     tsconfig: "./tsconfig.json",
-    sourcemap: true,
     format: 'esm',
     esbuildPlugins: [
         fixBuildPluginCjsImports(),
-        EsbuildPluginMeteorStubs,
     ],
     noExternal: ['meteor/isobuild', /meteor\//]
 });
