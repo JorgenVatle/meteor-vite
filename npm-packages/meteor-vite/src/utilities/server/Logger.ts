aaae724d02c8225732b5f9ce7c6ff0910b2ac5a4
Jørgen Vatle <Jorgen@WcAServices.net>
2025-07-07 14:37:34 +0300

    Correct import path for MeteorViteError

--- npm-packages/meteor-vite/src/utilities/server/Logger.ts
+++ npm-packages/meteor-vite/src/utilities/server/Logger.ts
@@ -1,4 +1,4 @@
-import { MeteorViteError } from '@/internals/error';
+import { MeteorViteError } from '@/internals/error/MeteorViteError';
 import pc from 'picocolors';
 
 function createLogger<Params extends DefaultParams>(formatter: (...params: Params) => DefaultParams): Logger<Params> {
