dba9638dea6c330fe411a56d4e4989546193abc7
Jørgen Vatle <Jorgen@WcAServices.net>
2025-07-07 16:35:02 +0300

    Add buildIfChanged method

--- build/has-changes.ts
+++ build/has-changes.ts
@@ -1,7 +1,19 @@
 import FS from 'fs/promises';
 import { globby } from 'globby';
 import { hash } from 'hasha';
+import { execSync } from 'node:child_process';
 import Path from 'node:path';
+
+export async function buildIfChanged(rootDir: string) {
+    const lastHash = await FS.readFile(Path.join(rootDir, '.build-hash'), 'utf8').catch(() => 'N/A');
+    const currentHash = await checkChanges(rootDir);
+    if (lastHash === currentHash) {
+        console.log('No changes detected, skipping build');
+        return;
+    }
+    console.log('Changes detected, running build');
+    execSync('npm run build', { cwd: rootDir, stdio: 'inherit' });
+}
 
 export async function checkChanges(rootDir: string) {
     const hash = await globHash([
