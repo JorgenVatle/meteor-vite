13f6c211be9175fb6847f0a75ac689d9207998d7
Jørgen Vatle <Jorgen@WcAServices.net>
2025-07-07 03:35:11 +0300

    Update BootstrapScripts runner for new meteor-vite internals package export

--- packages/vite/src/util/Bootstrap.ts
+++ packages/vite/src/util/Bootstrap.ts
@@ -1,11 +1,11 @@
-import type * as BootstrapScripts from 'meteor-vite/bootstrap/scripts';
+import type { Scripts as BootstrapScripts } from 'meteor-vite/internals';
+import { constants, Script } from 'node:vm';
 import { CurrentConfig } from './CurrentConfig';
-import { Script, constants } from 'node:vm';
 
 export function runBootstrapScript<
     TScript extends keyof typeof BootstrapScripts
 >(script: TScript): Promise<Awaited<ReturnType<typeof BootstrapScripts[TScript]>>> {
-    return new Script(`import('meteor-vite/bootstrap/scripts').then(scripts => scripts.${script}())`, {
+    return new Script(`import('meteor-vite/internals').then(({ scripts }) => scripts.${script}())`, {
         filename: CurrentConfig.bootstrapEvalFilename,
         importModuleDynamically: constants.USE_MAIN_CONTEXT_DEFAULT_LOADER,
     }).runInThisContext()
