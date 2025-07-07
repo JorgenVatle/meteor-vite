96e0d1bda3bbab3c606c93c737c80d8a27291526
Jørgen Vatle <Jorgen@WcAServices.net>
2025-07-07 14:25:24 +0300

    Use default minification options

--- build/defineBuildConfig.ts
+++ build/defineBuildConfig.ts
@@ -12,7 +12,6 @@ export function defineBuildConfig(rootDir: string, _options: Config | Config[]):
             sourcemap: true,
             dts: true,
             noExternal: ['meteor'],
-            minify: false,
         }), options, {
             outDir: Path.join(rootDir, options.outDir || 'dist'),
             tsconfig: options.tsconfig && Path.join(rootDir, options.tsconfig),
