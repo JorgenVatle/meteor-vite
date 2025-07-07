027158d5f641b88ecbc7444f2734b69dd8cdf7c8
Jørgen Vatle <Jorgen@WcAServices.net>
2025-07-07 12:01:18 +0300

    Resolve absolute paths for build defined build configs

--- build/defineBuildConfig.ts
+++ build/defineBuildConfig.ts
@@ -1,20 +1,57 @@
+import Path from 'path';
 import type { Options } from 'tsup';
 import { EsbuildPluginMeteorStubs } from './tsup-plugins';
 
-export function defineBuildConfig(config: Config): Options {
-    return Object.assign({
+export function defineBuildConfig(options: Config): Options {
+    const rootDir = inferConfigRootDir();
+    const config = Object.assign({
         target: 'es2022',
         sourcemap: true,
         dts: true,
         noExternal: ['meteor'],
         minify: false,
-    }, config, {
+    }, options, {
+        outDir: Path.resolve(rootDir, options.outDir || 'dist'),
         esbuildPlugins: [
             EsbuildPluginMeteorStubs,
-            ...config.esbuildPlugins || [],
+            ...options.esbuildPlugins || [],
         ]
     } satisfies Options)
+    
+    if (Array.isArray(config.entry)) {
+        config.entry = config.entry.map((entry) => Path.resolve(rootDir, entry));
+    } else {
+        const entries = Object.entries(config.entry).map(([key, path]) => [key, Path.resolve(rootDir, path)]);
+        config.entry = Object.fromEntries(entries);
+    }
+    
+    if (config.tsconfig) {
+        config.tsconfig = Path.resolve(rootDir, config.tsconfig);
+    }
+    
+    return config;
+}
+
+function inferConfigRootDir() {
+    const originalPrepareStackTrace = Error.prepareStackTrace;
+    
+    try {
+        const err = new Error();
+        Error.prepareStackTrace = (_, stack) => stack;
+        const stack = err.stack as unknown as NodeJS.CallSite[];
+        
+        // Skip frames until we find one that's not in this file
+        for (let i = 1; i < stack.length; i++) {
+            const fileName = stack[i].getFileName();
+            if (fileName && fileName !== __filename) {
+                return Path.dirname(fileName);
+            }
+        }
+    } finally {
+        Error.prepareStackTrace = originalPrepareStackTrace;
+    }
+    throw new Error('Unable to infer root directory for build config definition');
 }
 
 type RequiredConfigFields = Required<Pick<Options, 'name' | 'entry'>>;
-type Config = RequiredConfigFields & Pick<Options, 'entry'  | 'platform' | 'tsconfig' | 'format' | 'splitting' | 'dts' | 'clean' | 'onSuccess' | 'noExternal' | 'esbuildPlugins'>;
+type Config = RequiredConfigFields & Pick<Options, 'entry'  | 'platform' | 'tsconfig' | 'format' | 'splitting' | 'dts' | 'clean' | 'onSuccess' | 'noExternal' | 'esbuildPlugins' | 'outDir'>;
