18bb23bc148add6b11f3c0eb58812984a6af4c9d
Jørgen Vatle <Jorgen@WcAServices.net>
2025-07-09 17:18:20 +0300

    Assign dynamic asset boilerplate option to meteorSettings

--- npm-packages/meteor-vite/src/internals/lib/meteorSettings.ts
+++ npm-packages/meteor-vite/src/internals/lib/meteorSettings.ts
@@ -15,6 +15,7 @@ export const meteorSettings = {
     base: process.env.METEOR_VITE_BASE_URL || import.meta.env.BASE_URL,
     assetsDir: __VITE_ASSETS_DIR__,
     env: import.meta.env,
+    dynamicAssetBoilerplate: __VITE_DYNAMIC_ASSET_BOILERPLATE__,
 } as const;
 
 /**
