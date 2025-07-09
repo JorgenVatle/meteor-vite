13f9f1c9c0fe395d4f24db6bf642b845d06cd607
Jørgen Vatle <Jorgen@WcAServices.net>
2025-07-09 19:10:55 +0300

    Implement logger methods

--- npm-packages/meteor-vite/src/utilities/server/logger/LoggerInstance.ts
+++ npm-packages/meteor-vite/src/utilities/server/logger/LoggerInstance.ts
@@ -18,15 +18,42 @@ export class LoggerInstance {
         this.debugEnabled = ENABLE_DEBUG_LOGS || !!debugEnv.trim().split(/[\s,]+/).find((field) => {
             return debugTriggers.includes(field.trim())
         });
-        this.label = ['⚡  ', label].filter(Boolean).join(' ');
+        this.label = ['⚡  ', label].filter(Boolean).join('');
         this.colorizers = {
-            info: (message) => message,
-            success: (message) => message,
-            warn: (message) => message,
-            debug: (message) => message,
+            info: (message) => pc.blue(message),
+            success: (message) => pc.green(message),
+            warn: (message) => pc.yellow(message),
+            debug: (message) => pc.dim(pc.blue(message)),
             error: (message) => pc.dim(message),
         };
         Object.assign(this.colorizers, colorizers);
+    }
+    
+    protected log(level: Exclude<LoggerMethod, 'success'>, params: unknown[]) {
+        console[level](`${this.label} ${this.colorizers[level]('%s')}`, ...params)
+    }
+    
+    public info(...params: unknown[]) {
+        this.log('info', params);
+    }
+    
+    public success(...params: unknown[]) {
+        this.log('info', params);
+    }
+    
+    public warn(...params: unknown[]) {
+        this.log('warn', params);
+    }
+    
+    public error(...params: unknown[]) {
+        this.log('error', params);
+    }
+    
+    public debug(...params: unknown[]) {
+        if (!this.debugEnabled) {
+            return;
+        }
+        this.log('debug', params);
     }
     
 }
