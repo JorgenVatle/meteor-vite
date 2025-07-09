5f64b86ea0228537f9cc6820c8933d5bea1c6181
Jørgen Vatle <Jorgen@WcAServices.net>
2025-07-09 17:07:54 +0300

    Assign info about current Vite environment to Meteor settings

---
+++ npm-packages/meteor-vite/src/internals/lib/meteorSettings.ts
@@ -0,0 +1,27 @@
+import { Meteor } from 'meteor/meteor';
+
+/**
+ * Assign information about the current bundle to the app's runtime settings.
+ *
+ * This is necessary as there doesn't appear to be any good/reliable way to
+ * determine whether Meteor is running a Vite production bundle.
+ *
+ * With the production bundle, meteor-vite is bundled into the main application
+ * code and is no longer accessible. This lets us instruct the server runtime
+ * to not attempt to initialize any Vite modules since they're already
+ * included in the app at this point.
+ */
+export const meteorSettings = {
+    base: process.env.METEOR_VITE_BASE_URL || import.meta.env.BASE_URL,
+    assetsDir: __VITE_ASSETS_DIR__,
+    env: import.meta.env,
+}
+
+const userSettings = Meteor.settings.packages?.['jorgenvatle:vite'] || {}
+
+Meteor.settings.packages = Object.assign(
+    {
+        'jorgenvatle:vite': Object.assign(meteorSettings, userSettings)
+    },
+    Meteor.settings.packages
+);
