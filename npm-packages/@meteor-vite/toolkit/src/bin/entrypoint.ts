065e9be1ec3e7ce92e6e55d2934252e743b2cf7a
Jørgen Vatle <Jorgen@WcAServices.net>
2025-07-10 18:48:56 +0300

    Use command error classes for entrypoint

--- npm-packages/@meteor-vite/toolkit/src/bin/entrypoint.ts
+++ npm-packages/@meteor-vite/toolkit/src/bin/entrypoint.ts
@@ -1,14 +1,22 @@
 import { Commands } from '@/Commands';
+import { CommandFailure, CommandNotFound, MissingCommandArguments } from '@/errors/CommandFailure';
 
-const [_nodePath, _scriptPath, command, rootDir] = process.argv;
-
-if (!command) {
-    throw `You need to specify a command to run. E.g. ${_scriptPath} build-if-changed`;
+try {
+    const [_nodePath, _scriptPath, command, rootDir] = process.argv;
+    
+    if (!command) {
+        throw new CommandNotFound(`You need to specify a command to run. E.g. ${_scriptPath} build-if-changed`);
+    }
+    
+    if (!rootDir) {
+        throw new MissingCommandArguments(`You need to specify a root directory path to run this command. E.g. ${_scriptPath} ./npm-packages/meteor-vite`);
+    }
+    
+    await Commands.run(command, { rootDir });
+} catch (error) {
+    if (error instanceof CommandFailure) {
+        console.error(error.message);
+        process.exit(1);
+    }
+    throw error;
 }
-
-if (!rootDir) {
-    throw `You need to specify a root directory path to run this command. E.g. ${_scriptPath} ./npm-packages/meteor-vite`;
-}
-
-
-await Commands.run(command, { rootDir });
