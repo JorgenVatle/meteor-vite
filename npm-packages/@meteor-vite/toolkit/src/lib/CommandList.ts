b5d575f0654accd8ef52b106508aeeb9fbb9bca8
Jørgen Vatle <Jorgen@WcAServices.net>
2025-07-10 18:42:52 +0300

    Add CommandList class

---
+++ npm-packages/@meteor-vite/toolkit/src/lib/CommandList.ts
@@ -0,0 +1,30 @@
+export class CommandList<TCommand extends CommandSpec> {
+    constructor(protected readonly commands: TCommand[]) {
+    }
+    
+    public async run(commandName: TCommand['name'], args: CommandArgs) {
+        const command = this.get(commandName);
+        await command.handler(args);
+    }
+    
+    protected get(commandName: TCommand['name']) {
+        const command = this.commands.find((command) => command.name === commandName);
+        
+        if (!command) {
+            throw new Error(`Unknown command: ${commandName}`);
+        }
+        
+        return command;
+    }
+    
+}
+
+export type CommandSpec = {
+    name: string;
+    description: string;
+    handler: (args: CommandArgs) => Promise<void>;
+}
+
+type CommandArgs = {
+    rootDir: string;
+}
