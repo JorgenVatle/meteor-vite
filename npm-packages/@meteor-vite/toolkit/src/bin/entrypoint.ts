718d983276f5f88f67e3ccff076f958ec2f1b017
Jørgen Vatle <Jorgen@WcAServices.net>
2025-07-11 01:39:21 +0300

    Move CLI params parsing to dedicated function

--- npm-packages/@meteor-vite/toolkit/src/bin/entrypoint.ts
+++ npm-packages/@meteor-vite/toolkit/src/bin/entrypoint.ts
@@ -1,42 +1,16 @@
 import { Commands } from '@/Commands';
-import { CommandFailure, CommandNotFound, MissingCommandArguments } from '@/errors/CommandFailure';
-import type { CommandArgs } from '@/lib/CommandList';
+import { CommandFailure } from '@/errors/CommandFailure';
+import { parseCliParams } from '@/lib/parseCliParams';
 import * as process from 'node:process';
 
 try {
-    const { command, args } = parseParams();
+    const { command, options } = parseCliParams();
     
-    await Commands.run(command, args);
+    await Commands.run(command, options);
 } catch (error) {
     if (error instanceof CommandFailure) {
         console.error(error.message);
         process.exit(1);
     }
     throw error;
-}
-
-function parseParams(): Params {
-    const [_nodePath, _scriptPath, command, rootDir] = process.argv;
-    
-    
-    if (!command) {
-        throw new CommandNotFound(`You need to specify a command to run. E.g. ${_scriptPath} build-if-changed`);
-    }
-    
-    if (!rootDir) {
-        throw new MissingCommandArguments(`You need to specify a root directory path to run this command. E.g. ${_scriptPath} ./npm-packages/meteor-vite`);
-    }
-    
-    return {
-        command,
-        args: {
-            rootDir,
-            watch: process.argv.includes('--watch'),
-        }
-    }
-}
-
-interface Params {
-    command: string;
-    args: CommandArgs;
 }
--- npm-packages/@meteor-vite/toolkit/src/lib/parseCliParams.ts
+++ npm-packages/@meteor-vite/toolkit/src/lib/parseCliParams.ts
@@ -1,11 +1,32 @@
-export function parseCliParams(): { rootDir: string } {
-    const [_nodePath, _scriptPath, rootDir] = process.argv;
+import { CommandNotFound, MissingCommandArguments } from '@/errors/CommandFailure';
+import process from 'node:process';
+
+export function parseCliParams(params = process.argv): ParsedArgs {
+    const [_nodePath, _scriptPath, command, rootDir] = params;
+    
+    if (!command) {
+        throw new CommandNotFound(`You need to specify a command to run. E.g. ${_scriptPath} build-if-changed`);
+    }
     
     if (!rootDir) {
-        throw `You need to specify a root directory path to run this command. E.g. ${_scriptPath} ./npm-packages/meteor-vite`;
+        throw new MissingCommandArguments(`You need to specify a root directory path to run this command. E.g. ${_scriptPath} ./npm-packages/meteor-vite`);
     }
     
     return {
-        rootDir,
+        command,
+        options: {
+            rootDir,
+            watch: params.includes('--watch'),
+        }
     }
+}
+
+interface ParsedArgs {
+    command: string;
+    options: CommandOptions;
+}
+
+interface CommandOptions {
+    rootDir: string;
+    watch?: boolean;
 }
