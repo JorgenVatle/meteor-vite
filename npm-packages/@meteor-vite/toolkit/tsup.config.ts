be2f3d6d657e321379760477d20b2a7d58c30078
Jørgen Vatle <Jorgen@WcAServices.net>
2025-07-10 19:13:34 +0300

    Add build config for package

--- npm-packages/@meteor-vite/toolkit/tsup.config.ts
+++ npm-packages/@meteor-vite/toolkit/tsup.config.ts
@@ -1,12 +1,18 @@
 import { defineBuildConfig } from '@/defineBuildConfig';
 
-export default defineBuildConfig(import.meta.dirname, {
-    name: '@meteor-vite/toolkit/bin',
-    entry: ['src/bin'],
-    format: ['esm'],
-    dts: false,
-    clean: true,
-    banner: {
-        js: '#!/usr/bin/env node',
+export default defineBuildConfig(import.meta.dirname, [
+    {
+        name: '@meteor-vite/toolkit',
+        entry: ['src/index.ts'],
+    },
+    {
+        name: '@meteor-vite/toolkit: bin',
+        entry: ['src/bin'],
+        format: ['esm'],
+        dts: false,
+        clean: true,
+        banner: {
+            js: '#!/usr/bin/env node',
+        },
     }
-})
+]);
