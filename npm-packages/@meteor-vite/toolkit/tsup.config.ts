85dc21c0f3ebe0edb9dd20dbd1ac7a3a351f5329
Jørgen Vatle <Jorgen@WcAServices.net>
2025-07-10 23:01:36 +0300

    Build bin to dist directory, then copy to destination

--- npm-packages/@meteor-vite/toolkit/tsup.config.ts
+++ npm-packages/@meteor-vite/toolkit/tsup.config.ts
@@ -1,4 +1,5 @@
 import { defineBuildConfig } from '@/defineBuildConfig';
+import FS from 'fs/promises';
 
 export default defineBuildConfig(import.meta.dirname, [
     {
@@ -8,7 +9,13 @@ export default defineBuildConfig(import.meta.dirname, [
     {
         name: '@meteor-vite/toolkit: bin',
         entry: ['src/bin'],
-        outDir: 'bin',
+        outDir: 'dist/bin',
+        async onSuccess() {
+            await FS.cp('./dist/bin/', './bin/', {
+                recursive: true,
+            });
+            console.log('Copied ./dist/bin files to ./bin/');
+        },
         format: ['esm'],
         dts: false,
         banner: {
