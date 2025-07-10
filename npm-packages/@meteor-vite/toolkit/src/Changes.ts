4e2c08727a0e359244108f5018354beb5d22ffdf
Jørgen Vatle <Jorgen@WcAServices.net>
2025-07-10 18:30:48 +0300

    Refactor changes utility to use a class structure instead of individual functions

--- npm-packages/@meteor-vite/toolkit/src/Changes.ts
+++ npm-packages/@meteor-vite/toolkit/src/Changes.ts
@@ -4,45 +4,52 @@ import { hash } from 'hasha';
 import { execSync } from 'node:child_process';
 import Path from 'node:path';
 
-export async function buildIfChanged(rootDir: string, options?: Options) {
-    const lastHash = await FS.readFile(Path.join(rootDir, '.build-hash'), 'utf8').catch(() => 'N/A');
-    const currentHash = await checkChanges(rootDir, options);
-    if (lastHash === currentHash) {
-        console.log('No changes detected, skipping build');
-        return;
-    }
-    console.log('Changes detected, running build');
-    execSync('npm run build', {
-        cwd: rootDir,
-        stdio: 'inherit',
-        env: Object.assign({
-            TSUP_CLEAN: true,
-        }, process.env),
-    });
-}
-
-export async function checkChanges(rootDir: string, options?: Options) {
-    const hash = await globHash({
-        fileContent: [
-            Path.join(rootDir, 'src'),
-            Path.join(rootDir, 'tsconfig.json'),
-        ],
-        // Used to trigger a re-build if the dist directory is deleted or
-        // partially built.
-        fileNames: [
-            Path.join(rootDir, 'dist')
-        ]
-    }, options);
+export class Changes {
+    constructor(
+        protected readonly rootDir: string,
+        protected readonly options: Options = {
+            saveBuildHash: true,
+        }
+    ) {}
     
-    if (options?.saveBuildHash ?? true) {
-        await FS.writeFile(Path.join(rootDir, '.build-hash'), hash);
+    public async buildIfChanged() {
+        const lastHash = await FS.readFile(Path.join(this.rootDir, '.build-hash'), 'utf8').catch(() => 'N/A');
+        const currentHash = await this.checkChanges();
+        if (lastHash === currentHash) {
+            console.log('No changes detected, skipping build');
+            return;
+        }
+        console.log('Changes detected, running build');
+        execSync('npm run build', {
+            cwd: this.rootDir,
+            stdio: 'inherit',
+            env: Object.assign({
+                TSUP_CLEAN: true,
+            }, process.env),
+        });
     }
     
-    return hash;
-}
-
-async function globHash(
-    patterns: {
+    public async checkChanges() {
+        const hash = await this.globHash({
+            fileContent: [
+                Path.join(this.rootDir, 'src'),
+                Path.join(this.rootDir, 'tsconfig.json'),
+            ],
+            // Used to trigger a re-build if the dist directory is deleted or
+            // partially built.
+            fileNames: [
+                Path.join(this.rootDir, 'dist')
+            ]
+        });
+        
+        if (this.options.saveBuildHash ?? true) {
+            await FS.writeFile(Path.join(this.rootDir, '.build-hash'), hash);
+        }
+        
+        return hash;
+    }
+    
+    protected async globHash(patterns: {
         /**
          * Glob patterns for files and directories to read and include in the hash.
          */
@@ -53,45 +60,45 @@ async function globHash(
          * directories are empty or only partially built. (Glob patterns supported)
          */
         fileNames?: string[]
-    },
-    options: Options = {}
-) {
-    const startTime = Date.now();
-    const files = await globby(patterns.fileContent);
-    const fileNames = await globby(patterns.fileNames || []);
-    
-    files.sort();
-    fileNames.sort();
-    
-    const contentHashes = await Promise.all(files.map(async (file) => {
-        const buffer = await FS.readFile(file);
-        return hash(buffer, { algorithm: 'md5' });
-    }));
-    
-    const fileNameHashes = await Promise.all(fileNames.map(async (fileName) => {
-        return hash(fileName, { algorithm: 'md5' });
-    }));
-    
-    
-    console.log('\n');
-    
-    console.log([
-        `Computed ${contentHashes.length} content and ${fileNameHashes.length} file name hashes for`,
-        `build in ${Date.now() - startTime}ms`,
-    ].join(' '));
-    
-    
-    const result = await hash([contentHashes, fileNameHashes].flat().join(), { algorithm: 'sha1' });
-    console.log(`Hash: ${result}`);
-    
-    if (options.detailedLogging) {
-        console.log({ files, fileNames });
+    }) {
+        const startTime = Date.now();
+        const files = await globby(patterns.fileContent);
+        const fileNames = await globby(patterns.fileNames || []);
+        
+        files.sort();
+        fileNames.sort();
+        
+        const contentHashes = await Promise.all(files.map(async (file) => {
+            const buffer = await FS.readFile(file);
+            return hash(buffer, { algorithm: 'md5' });
+        }));
+        
+        const fileNameHashes = await Promise.all(fileNames.map(async (fileName) => {
+            return hash(fileName, { algorithm: 'md5' });
+        }));
+        
+        
+        console.log('\n');
+        
+        console.log([
+            `Computed ${contentHashes.length} content and ${fileNameHashes.length} file name hashes for`,
+            `build in ${Date.now() - startTime}ms`,
+        ].join(' '));
+        
+        
+        const result = await hash([contentHashes, fileNameHashes].flat().join(), { algorithm: 'sha1' });
+        console.log(`Hash: ${result}`);
+        
+        if (this.options.detailedLogging) {
+            console.log({ files, fileNames });
+        }
+        
+        console.log('\n');
+        
+        return result;
     }
-    
-    console.log('\n');
-    
-    return result;
 }
+
 
 type Options = {
     detailedLogging?: boolean;
