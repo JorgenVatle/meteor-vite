d597515470bf0909d900705e5b535f15e1b5edac
Jørgen Vatle <Jorgen@WcAServices.net>
2025-07-10 19:20:58 +0300

    Import defineBuildConfig from local toolkit package

--- npm-packages/meteor-vite/tsup.config.ts
+++ npm-packages/meteor-vite/tsup.config.ts
@@ -1,6 +1,6 @@
+import { defineBuildConfig } from '@meteor-vite/toolkit';
 import FS from 'fs';
 import Path from 'path';
-import { defineBuildConfig } from '../../build/src/defineBuildConfig';
 
 let clean = false;
 
