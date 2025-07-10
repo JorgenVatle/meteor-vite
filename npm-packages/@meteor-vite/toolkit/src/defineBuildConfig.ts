017e575ff8cbcb7a55f9d37a2c50d704813991d6
Jørgen Vatle <Jorgen@WcAServices.net>
2025-07-10 19:18:37 +0300

    Add option for skipping node modules bundle

--- npm-packages/@meteor-vite/toolkit/src/defineBuildConfig.ts
+++ npm-packages/@meteor-vite/toolkit/src/defineBuildConfig.ts
@@ -75,4 +75,4 @@ function inferConfigRootDir() {
 }
 
 type RequiredConfigFields = Required<Pick<Options, 'name' | 'entry'>>;
-type Config = RequiredConfigFields & Pick<Options, 'entry' | 'sourcemap' | 'banner' | 'platform' | 'tsconfig' | 'format' | 'splitting' | 'dts' | 'clean' | 'onSuccess' | 'noExternal' | 'esbuildPlugins' | 'outDir'>;
+type Config = RequiredConfigFields & Pick<Options, 'entry' | 'skipNodeModulesBundle' | 'sourcemap' | 'banner' | 'platform' | 'tsconfig' | 'format' | 'splitting' | 'dts' | 'clean' | 'onSuccess' | 'noExternal' | 'esbuildPlugins' | 'outDir'>;
