47623dbf539160d7ce22c92b61180eab93c35a13
Jørgen Vatle <Jorgen@WcAServices.net>
2025-07-11 00:41:08 +0300

    Remove unnecessary intermediary copy function

--- npm-packages/@meteor-vite/toolkit/src/buildConfig/copyFilesPlugin.ts
+++ npm-packages/@meteor-vite/toolkit/src/buildConfig/copyFilesPlugin.ts
@@ -1,4 +1,4 @@
-import type { Config, CopyConfig, TSUpPlugin } from '@/buildConfig/defineBuildConfig';
+import type { Config, TSUpPlugin } from '@/buildConfig/defineBuildConfig';
 import FS from 'fs/promises';
 import Path from 'path';
 
@@ -14,37 +14,28 @@ export function copyFilesPlugin(rootDir: string, config: Config): TSUpPlugin {
             
             await Promise.all(
                 filesToCopy.map(async (copy) => {
-                    this.logger.info(`Copy ${copy.type}:`, `${copy.from} -> ${copy.to}`);
+                    const srcPath = Path.join(rootDir, copy.from);
+                    const destPath = Path.join(rootDir, copy.to);
                     
-                    await copyFiles({
-                        rootDir,
-                        copy
+                    this.logger.info(`Copy ${copy.type}:`, copy.from, '->', copy.to);
+                    
+                    await FS.access(srcPath).catch((error) => {
+                        throw new Error(`Could not locate source file: ${srcPath}`);
                     });
+                    await FS.mkdir(Path.dirname(destPath), { recursive: true });
+                    
+                    switch (copy.type) {
+                        case 'directory':
+                            await FS.cp(srcPath, destPath, { recursive: true });
+                            return;
+                        case 'file':
+                            await FS.copyFile(srcPath, destPath);
+                            return;
+                    }
+                    
+                    throw new Error(`Unknown copy type: ${copy.type}`);
                 })
             );
         }
     }
-}
-
-type FileCopyOptions = {
-    rootDir: string;
-    copy: CopyConfig;
-}
-
-async function copyFiles({ rootDir, copy }: FileCopyOptions) {
-    const srcPath = Path.join(rootDir, copy.from);
-    const destPath = Path.join(rootDir, copy.to);
-    
-    await FS.mkdir(Path.dirname(destPath), { recursive: true });
-    
-    switch (copy.type) {
-        case 'directory':
-            await FS.cp(srcPath, destPath, { recursive: true });
-            return;
-        case 'file':
-            await FS.copyFile(srcPath, destPath);
-            return;
-    }
-    
-    throw new Error(`Unknown copy type: ${copy.type}`);
 }
