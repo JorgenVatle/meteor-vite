9bdb5c02f2af64c8305c52d13c20bc086af92db7
Jørgen Vatle <Jorgen@WcAServices.net>
2025-07-11 10:18:56 +0300

    Shorten environment mapped module key

--- npm-packages/meteor-vite/src/internals/lib/setup/setupMainModules.ts
+++ npm-packages/meteor-vite/src/internals/lib/setup/setupMainModules.ts
@@ -1,28 +1,28 @@
 import { internalEntryModule, type MainModule } from '@/internals/lib/internalEntryModule';
 
 export function setupMainModules(mainModule: { vite: MainModule, meteor: MainModule }) {
-    const { vite, meteor } = internalEntryModule();
+    const { meteor, vite: { development, production } } = internalEntryModule();
     
     {
         // [PROD] Vite Client
-        vite.production.client.addImport({ path: 'vite/modulepreload-polyfill' });
-        vite.production.client.addImport({ path: mainModule.vite.client.path });
-        vite.production.client.write();
+        production.client.addImport({ path: 'vite/modulepreload-polyfill' });
+        production.client.addImport({ path: mainModule.vite.client.path });
+        production.client.write();
         
         // [PROD] Vite Server
-        vite.production.server.addImport({ path: 'meteor-vite/server-entry/production' });
-        vite.production.server.addImport({ path: mainModule.vite.server.path });
-        vite.production.server.write();
+        production.server.addImport({ path: 'meteor-vite/server-entry/production' });
+        production.server.addImport({ path: mainModule.vite.server.path });
+        production.server.write();
     }
     
     {
         // [DEV] Vite Client
-        vite.development.client.addImport({ path: mainModule.vite.client.path });
-        vite.development.client.write();
+        development.client.addImport({ path: mainModule.vite.client.path });
+        development.client.write();
         
         // [DEV] Vite Server
-        vite.development.server.addImport({ path: 'meteor-vite/server-entry/hmr' });
-        vite.development.server.addImport({ path: mainModule.vite.server.path });
-        vite.development.server.write();
+        development.server.addImport({ path: 'meteor-vite/server-entry/hmr' });
+        development.server.addImport({ path: mainModule.vite.server.path });
+        development.server.write();
     }
 }
