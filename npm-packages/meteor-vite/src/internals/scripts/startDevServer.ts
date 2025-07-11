2afd3011e7267e68f46f49f8690ec9e528ba3d2f
Jørgen Vatle <Jorgen@WcAServices.net>
2025-07-11 08:22:31 +0300

    Move redundant development server entry into start script

--- npm-packages/meteor-vite/src/internals/scripts/startDevServer.ts
+++ npm-packages/meteor-vite/src/internals/scripts/startDevServer.ts
@@ -1,5 +1,9 @@
 /// <reference types="vite/client" />
+import Instance from '@/internals/lib/MeteorViteRuntime';
+import { resolveMeteorViteConfig } from '@/internals/lib/resolveMeteorViteConfig';
 import { ViteBundleLogger as Logger } from '@/utilities/server';
+import { WebApp } from 'meteor/webapp';
+import { createServer, createServerModuleRunner } from 'vite';
 
 export async function startDevServer() {
     /**
@@ -14,10 +18,35 @@ export async function startDevServer() {
         return;
     }
     
-    await import ('@/internals/server-entry/development').catch((error) => {
-        Logger.warn('Failed to start Vite dev server!');
-        throw error;
-    });
+    const { config, modules } = await resolveMeteorViteConfig({
+        mode: 'development',
+    }, 'serve');
+    
+    const server = await createServer(config);
+    
+    await server.warmupRequest(modules.clientEntry);
+    
+    // ⚡ [Server] Transform and load the Meteor main module using Vite.
+    if (modules.serverEntry) {
+        const runner = createServerModuleRunner(server.environments.server);
+        Instance.logger.info(`Loading server entry: ${modules.serverEntry}`);
+        
+        // HMR listener to clean up side-effects from things like
+        // Meteor.publish(), new Mongo.Collection(), etc. on server-side hot reload.
+        try {
+            await runner.import('meteor-vite/server-entry/hmr');
+            await runner.import(modules.serverEntry);
+        } catch (error) {
+            if (error instanceof Error) {
+                server.ssrFixStacktrace(error);
+            }
+            throw error;
+        }
+    }
+    
+    // ⚡ [Vite] Bind Vite to Meteor's Express app to serve modules and assets to clients.
+    WebApp.handlers.use(server.middlewares);
+    Instance.printUrls(config);
     
     Logger.success('Vite should be ready to go!');
 }
