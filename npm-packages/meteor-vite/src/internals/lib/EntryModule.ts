844da156406f6d3c339e07b646ef7b0cbb1058ff
Jørgen Vatle <Jorgen@WcAServices.net>
2025-07-11 08:57:33 +0300

    Add EntryModule class for managing imports for a given module path.

---
+++ npm-packages/meteor-vite/src/internals/lib/EntryModule.ts
@@ -0,0 +1,32 @@
+import { writeToPathSync } from '@/internals/lib/writeToPathSync';
+import { moduleImport } from '@/utilities/server';
+import Path from 'path';
+
+export class EntryModule {
+    protected readonly imports: ModuleImport[] = [];
+    constructor(protected readonly path: string) {
+    
+    }
+    
+    public addImport(module: ModuleImport) {
+        this.imports.push(module);
+    }
+    
+    public async write() {
+        const importLines = this.imports.map(({ path }) => {
+            // Importing a file directly (not a node module)
+            if (path.startsWith('.') || path.startsWith('/')) {
+                return moduleImport(Path.relative(this.path, path))
+            }
+            
+            // Importing a package from node_modules
+            return moduleImport(path);
+        });
+        
+        writeToPathSync(this.path, importLines.join('\n'));
+    }
+}
+
+type ModuleImport = {
+    path: string;
+}
