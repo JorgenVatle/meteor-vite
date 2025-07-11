35321b0e6289bc8e78f3dcd7e290f9d13b7570c2
Jørgen Vatle <Jorgen@WcAServices.net>
2025-07-11 10:53:21 +0300

    Add main module resolver function

---
+++ npm-packages/meteor-vite/src/internals/lib/setup/resolveMainModules.ts
@@ -0,0 +1,28 @@
+import { MeteorViteError } from '@/internals/error/MeteorViteError';
+import { EntryModule } from '@/internals/lib/EntryModule';
+import type { MainModule } from '@/internals/lib/internalEntryModule';
+import { setupMainModules } from '@/internals/lib/setup/setupMainModules';
+import type { ProjectJson, ResolvedViteConfig } from '@/plugin';
+
+export function resolveMainModules({ packageJson, userConfig }: { userConfig: ResolvedViteConfig, packageJson: ProjectJson }) {
+    const mainModulePath = packageJson.meteor.mainModule;
+    
+    if (!mainModulePath.server) {
+        throw new MeteorViteError('package.json is missing a server mainModule. Make sure the meteor.mainModule.server field is set your package.json');
+    }
+    
+    const meteor: MainModule = {
+        server: new EntryModule(mainModulePath.server),
+        client: new EntryModule(mainModulePath.client),
+    }
+    
+    const vite: MainModule = {
+        server: new EntryModule(userConfig.meteor?.serverEntry),
+        client: new EntryModule(userConfig.meteor?.clientEntry),
+    };
+    
+    return setupMainModules({
+        vite,
+        meteor,
+    })
+}
