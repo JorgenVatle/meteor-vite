b76b2ba82851082a0cd8c5b086d98f6f7b476d74
Jørgen Vatle <Jorgen@WcAServices.net>
2025-07-11 09:28:15 +0300

    Refactor main module setup to use block scoping instead of individual functions

--- npm-packages/meteor-vite/src/internals/lib/setup/setupMainModules.ts
+++ npm-packages/meteor-vite/src/internals/lib/setup/setupMainModules.ts
@@ -1,37 +1,32 @@
 import { internalEntryModule, type MainModule } from '@/internals/lib/internalEntryModule';
 
 export function setupMainModules(mainModule: MainModule) {
-    addDevelopmentImports(mainModule);
-    addProductionImports(mainModule);
-}
-
-function addProductionImports(mainModule: MainModule) {
-    const { production: { client, server } } = internalEntryModule();
+    {
+        const { client, server } = internalEntryModule().production;
+        
+        // [PROD] Client
+        client.vite.addImport({ path: 'vite/modulepreload-polyfill' });
+        client.vite.addImport({ path: mainModule.client.vite.path });
+        
+        // [PROD] Server
+        server.vite.addImport({ path: 'meteor-vite/server-entry/production' });
+        server.vite.addImport({ path: mainModule.server.vite.path });
+        
+        client.vite.write();
+        server.vite.write();
+    }
     
-    // Client
-    client.vite.addImport({ path: 'vite/modulepreload-polyfill' });
-    client.vite.addImport({ path: mainModule.client.vite.path });
-    
-    // Server
-    server.vite.addImport({ path: 'meteor-vite/server-entry/production' });
-    server.vite.addImport({ path: mainModule.server.vite.path });
-    
-    // Write files
-    client.vite.write();
-    server.vite.write();
-}
-
-function addDevelopmentImports(mainModule: MainModule) {
-    const { development: { client, server } } = internalEntryModule();
-    
-    // Client
-    client.vite.addImport({ path: mainModule.client.vite.path });
-    
-    // Server
-    server.vite.addImport({ path: 'meteor-vite/server-entry/hmr' });
-    server.vite.addImport({ path: mainModule.server.vite.path });
-    
-    // Write files
-    client.vite.write();
-    server.vite.write();
+    {
+        const { client, server } = internalEntryModule().development;
+        
+        // [DEV] Client
+        client.vite.addImport({ path: mainModule.client.vite.path });
+        
+        // [DEV] Server
+        server.vite.addImport({ path: 'meteor-vite/server-entry/hmr' });
+        server.vite.addImport({ path: mainModule.server.vite.path });
+        
+        client.vite.write();
+        server.vite.write();
+    }
 }
