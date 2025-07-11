e55a53ddf392542de6c0813a999f7ee61ab8efb8
Jørgen Vatle <Jorgen@WcAServices.net>
2025-07-11 11:36:25 +0300

    Assign replaced patch template

--- npm-packages/meteor-vite/src/internals/lib/EntryModule.ts
+++ npm-packages/meteor-vite/src/internals/lib/EntryModule.ts
@@ -58,7 +58,7 @@ export class EntryModule {
         const TERMINATION_LINE = `/** End of vite auto-imports **/`;
         let patchedContent = originalContent;
         OLD_TERMINATION_LINES.forEach(line => {
-            patchedContent.replace(line, TERMINATION_LINE);
+            patchedContent = patchedContent.replace(line, TERMINATION_LINE);
         })
         if (!originalContent.includes(TERMINATION_LINE)) {
             patchedContent = [
