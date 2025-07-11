09140920de19f4be439bb32b3d0cbf0abdc7fb5b
Jørgen Vatle <Jorgen@WcAServices.net>
2025-07-11 07:39:39 +0300

    Define build config using toolkit

--- npm-packages/@meteor-vite/plugin-zodern-relay/tsup.config.ts
+++ npm-packages/@meteor-vite/plugin-zodern-relay/tsup.config.ts
@@ -1,12 +1,5 @@
-import { defineConfig } from 'tsup';
+import { defineBuildConfig } from '@meteor-vite/toolkit';
 
-export default defineConfig({
-    target: 'es2022',
-    outDir: 'dist',
+export default defineBuildConfig({
     entry: ['src/Plugin.ts'],
-    sourcemap: true,
-    dts: true,
-    format: ['cjs', 'esm'],
-    skipNodeModulesBundle: true,
-    clean: true,
 })
