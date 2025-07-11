4251e5b5123a0dc412365f7006915082aa17fcf0
Jørgen Vatle <Jorgen@WcAServices.net>
2025-07-11 14:22:38 +0300

    Add imports for build output to internal meteor main modules

--- npm-packages/meteor-vite/src/internals/lib/setup/setupMainModules.ts
+++ npm-packages/meteor-vite/src/internals/lib/setup/setupMainModules.ts
@@ -1,7 +1,7 @@
 import { internalEntryModule, type MainModule, type ViteMainModule } from '@/internals/lib/internalEntryModule';
 
-export function setupMainModules(mainModule: { vite: ViteMainModule, meteor: MainModule }) {
-    const { meteor, vite: { development, production } } = internalEntryModule();
+export function setupMainModules(mainModule: { vite: ViteMainModule, meteor: MainModule, buildOutput: MainModule }) {
+    const { meteor, vite: { development, production}, buildOutput } = internalEntryModule;
     
     // [Vite Client]
     production.client.addImport({ path: 'vite/modulepreload-polyfill' });
@@ -22,6 +22,15 @@ export function setupMainModules(mainModule: { vite: ViteMainModule, meteor: Mai
         development.server.write();
     }
     
+    // [Build Output]
+    {
+        meteor.client.addImport({ path: buildOutput.client.path });
+        meteor.client.write();
+        
+        meteor.server.addImport({ path: buildOutput.server.path });
+        meteor.server.write();
+    }
+    
     // [Meteor Main Module]
     // Add internal module imports to app's Meteor mainModule.
     // This lets us un-lazy-load new packages by adding imports to our internal
@@ -29,12 +38,10 @@ export function setupMainModules(mainModule: { vite: ViteMainModule, meteor: Mai
     // apart from this one time here.
     {
         // Client
-        meteor.client.prepare();
         mainModule.meteor.client.addImport({ path: meteor.client.path });
         mainModule.meteor.client.appendMissing();
         
         // Server
-        meteor.server.prepare();
         mainModule.meteor.server.addImport({ path: meteor.server.path });
         mainModule.meteor.server.appendMissing();
     }
