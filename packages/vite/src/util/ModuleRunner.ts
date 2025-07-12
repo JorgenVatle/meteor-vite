4654857647177d77b52051d1de57bc1332c9782f
Jørgen Vatle <Jorgen@WcAServices.net>
2025-07-13 01:22:16 +0300

    Refactor script declaration to make it a little easier to read

--- packages/vite/src/util/ModuleRunner.ts
+++ packages/vite/src/util/ModuleRunner.ts
@@ -2,7 +2,17 @@ import { type ModuleRunner as InternalModuleRunner } from 'meteor-vite/internals
 import { constants, Script } from 'node:vm';
 import { CurrentConfig } from './CurrentConfig';
 
-export const ModuleRunner: InternalModuleRunner = await new Script(`import('meteor-vite/internals').then(({ ModuleRunner }) => new ModuleRunner())`, {
-    filename: CurrentConfig.bootstrapEvalFilename,
-    importModuleDynamically: constants.USE_MAIN_CONTEXT_DEFAULT_LOADER,
-}).runInThisContext();
+// language=javascript
+const script = `
+    import('meteor-vite/internals').then(({ ModuleRunner }) => {
+        return new ModuleRunner();
+    })
+`
+
+export const ModuleRunner: InternalModuleRunner = await new Script(
+    script,
+    {
+        filename: CurrentConfig.bootstrapEvalFilename,
+        importModuleDynamically: constants.USE_MAIN_CONTEXT_DEFAULT_LOADER,
+    },
+).runInThisContext();
