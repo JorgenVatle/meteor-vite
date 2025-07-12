c61cd645a5cadf57dc3aa694ea075fd630c12156
Jørgen Vatle <Jorgen@WcAServices.net>
2025-07-13 01:19:32 +0300

    Add CLI utility for rewriting file URLs for WSL paths.

---
+++ npm-packages/@meteor-vite/toolkit/src/bin/rewrite-file-urls.ts
@@ -0,0 +1,37 @@
+/**
+ * Rewrite file URLs to handle stack traces with absolute paths that don't
+ * correctly map to the current filesystem.
+ *
+ * Primarily to map e.g. WSL file paths to Windows-compatible filepath.
+ *
+ * @example Usage from the terminal
+ * node ./bar.js  2>&1 | rewrite-file-urls '//wsl$/Ubuntu'
+ *
+ * @example Stack trace
+ * // input (Not resolvable by Windows)
+ * Error: ...
+ * at (file:///home/foo/bar.js:13:37)
+ *
+ * // output (Valid Windows path)
+ * Error: ...
+ * at (file:///wsl$/Ubuntu/foo/bar.js:13:37)
+ */
+
+import Readline from 'readline';
+
+const [nodePath, scriptPath, replacement = '//wsl.localhost/Ubuntu'] = process.argv;
+
+const readline = Readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    terminal: false,
+});
+
+console.log([
+    '\n',
+    `[Rewriting file URLs to: file://${replacement}/]`,
+    '\n',
+].join('\n'));
+readline.on('line', (line) => {
+    console.log(line.replace('file:///', `file://${replacement}/`));
+});
