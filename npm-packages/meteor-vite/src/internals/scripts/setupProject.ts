04d3892de61275242e7548399571f20ea5cccfbc
Jørgen Vatle <Jorgen@WcAServices.net>
2025-07-12 21:12:55 +0300

    Correct internal module cleanup

--- npm-packages/meteor-vite/src/internals/scripts/setupProject.ts
+++ npm-packages/meteor-vite/src/internals/scripts/setupProject.ts
@@ -1,4 +1,4 @@
-import { internalEntryModule } from '@/internals/lib/internalEntryModule';
+import { getInternalModules } from '@/internals/lib/EntryModule/helpers/get';
 import { meteorSettings } from '@/internals/lib/meteorSettings';
 import { CurrentConfig } from '@/internals/lib/resolveMeteorViteConfig';
 import { writeToPathSync } from '@/internals/lib/writeToPathSync';
@@ -56,7 +56,8 @@ function cleanupPreviousBuilds() {
         return;
     }
     FS.rmSync(CurrentConfig.outDir, { recursive: true, force: true });
-    internalEntryModule().buildOutput.client.clean();
-    internalEntryModule().buildOutput.server.clean();
+    const { buildOutput } = getInternalModules();
+    buildOutput.client.clean();
+    buildOutput.server.clean();
     logger.info(`Cleaned up old build output in ${pc.green(CurrentConfig.outDir)}`);
 }
