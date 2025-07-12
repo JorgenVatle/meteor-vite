5099ec86abea41e3c313ca29ad3ca1cc30e18551
Jørgen Vatle <Jorgen@WcAServices.net>
2025-07-12 20:20:27 +0300

    Add ViteEntryModule class where writes are prevented.

---
+++ npm-packages/meteor-vite/src/internals/lib/EntryModule/ViteEntryModule.ts
@@ -0,0 +1,18 @@
+import { MeteorViteError } from '@/internals/error/MeteorViteError';
+import { EntryModuleBase, type EntryModuleConfig } from '@/internals/lib/EntryModule/EntryModuleBase';
+import { bugs } from '@/utilities/common/Constants';
+
+export class ViteEntryModule extends EntryModuleBase {
+    public readonly config: EntryModuleConfig = {
+        location: 'app-source',
+    };
+    
+    protected _write() {
+        throw new MeteorViteError('Tried to write to a Vite entry module.', {
+            subtitle: [
+                `This should never happen and most likely is a bug in Meteor-Vite.`,
+                `Please report this issue at ${bugs.url}.`
+            ].join(' '),
+        })
+    }
+}
