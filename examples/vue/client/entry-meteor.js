9107d0801844cddf2db5f4b6da7467c171a978d0
Jørgen Vatle <Jorgen@WcAServices.net>
2025-07-13 19:53:25 +0300

    Update entry modules for Vite example

--- examples/vue/client/entry-meteor.js
+++ examples/vue/client/entry-meteor.js
@@ -5,12 +5,14 @@
  *
  * More info: https://github.com/JorgenVatle/meteor-vite#lazy-loaded-meteor-packages
 **/
+import '../_vite-bundle/client/_entry-meteor.mjs';
+/** End of vite auto-imports **/
 import 'meteor/test:lazy';
 import 'meteor/test:ts-modules/re-exports-index';
 import 'meteor/test:ts-modules/export-star-from';
 import 'meteor/test:stub-validation';
 import 'meteor/test:duplicate-npm-dependencies';
-/** End of vite-bundler auto-imports **/
+
 import { Cookies } from 'meteor/ostrio:cookies';
 
 const cookies = new Cookies()
