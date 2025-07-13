1ce72ec99c20deb59b0bad3a6b4261b47e779791
Jørgen Vatle <Jorgen@WcAServices.net>
2025-07-13 20:46:23 +0300

    Assert that home page content is rendered

--- tests/cypress-suite/cypress/e2e/examples/vue/spec.cy.ts
+++ tests/cypress-suite/cypress/e2e/examples/vue/spec.cy.ts
@@ -1,5 +1,10 @@
 describe('Vue example app', () => {
     it('can load the landing page', () => {
         cy.visit('/')
+    });
+    
+    it('Renders home-page content from Vue', () => {
+        cy.visit('/');
+        cy.contains('h1', 'Home');
     })
 })
