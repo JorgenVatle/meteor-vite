7232568af040f5bfa4a530fd5c5db78b55a5536d
Jørgen Vatle <Jorgen@WcAServices.net>
2025-07-13 13:46:37 +0300

    Add command concurrency manager

---
+++ npm-packages/@meteor-vite/toolkit/src/lib/CommandConcurrency.ts
@@ -0,0 +1,39 @@
+import { concurrently, type ConcurrentlyCommandInput, type ConcurrentlyOptions } from 'concurrently';
+
+export class CommandConcurrency {
+    constructor(
+        protected readonly commands: ConcurrentCommand[] = []
+    ) {};
+    
+    protected readonly defaultOptions: Partial<ConcurrentlyOptions> = {
+        killOthersOn: 'failure',
+    };
+    
+    public add(command: ConcurrentCommand) {
+        this.commands.push(command);
+    };
+    
+    protected formatArgs(args: string[]) {
+        return args.map((arg) => JSON.stringify(arg))
+    }
+    
+    public run(options?: Partial<ConcurrentlyOptions>) {
+        const commands: CommandInfo[] = this.commands.map(({ command, args, ...options }) => {
+            return {
+                command: [command, this.formatArgs(args)].flat().join(' '),
+                ...options,
+            }
+        });
+        const defaultOptions: Partial<ConcurrentlyOptions> = {}
+        concurrently(commands, {
+            ...this.defaultOptions,
+            ...options,
+        });
+    }
+}
+
+type CommandInfo = Extract<ConcurrentlyCommandInput, { command: string }>;
+type ConcurrentCommand = {
+    command: string;
+    args: string[];
+} & CommandInfo;
