98e7f22db79e643d1e8d48e0357d04c082495594
Jørgen Vatle <Jorgen@WcAServices.net>
2025-07-13 12:52:46 +0300

    Implement concurrent build commands

--- npm-packages/@meteor-vite/toolkit/src/Commands.ts
+++ npm-packages/@meteor-vite/toolkit/src/Commands.ts
@@ -2,6 +2,7 @@ import { Command } from '@/lib/Command';
 import { CommandList } from '@/lib/CommandList';
 import { Highlight } from '@/lib/Highlight';
 import { ProjectCompiler } from '@/ProjectCompiler';
+import { concurrently } from 'concurrently';
 
 export const Commands = new CommandList([
     new Command('check-changes', {
@@ -13,9 +14,51 @@ export const Commands = new CommandList([
     }),
     new Command('build', {
         description: 'Run a build and compute the build hash for the provided project root directory.',
-        fields: {},
-        handler: async () => {
-            await ProjectCompiler.init().build();
+        fields: {
+            concurrent: {
+                type: String,
+                description: 'Run the same command in parallel within the root directories specified',
+                multiple: true,
+                optional: true,
+                typeLabel: 'rootDir1 rootDir2 ...',
+            },
+            run: {
+                type: String,
+                description: 'Runs the provided command in a separate process concurrently with the build.',
+                multiple: true,
+                optional: true,
+            },
+        },
+        handler: async (options) => {
+            const commands: { command: string, arguments: string[] }[] = [];
+            if (options.concurrent) {
+                options.concurrent.forEach(rootDir => {
+                    commands.push({
+                        command: process.argv[1],
+                        arguments: ['build', rootDir, '--watch']
+                    })
+                })
+            }
+            if (options.run) {
+                const [command, ...args] = options.run;
+                commands.push({
+                    command,
+                    arguments: args,
+                });
+            }
+            if (!commands.length) {
+                await ProjectCompiler.init().build();
+                return;
+            }
+            try {
+                await concurrently(commands, {
+                    prefix: 'none',
+                    restartTries: 0,
+                    killOthers: ['failure'],
+                }).result
+            } catch (error) {
+                console.error(error);
+            }
         }
     }),
     new Command('clean', {
