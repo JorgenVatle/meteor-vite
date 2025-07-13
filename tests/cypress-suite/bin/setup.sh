a3193744d44d6e04f1f458348980742b3c5d8eda
Jørgen Vatle <Jorgen@WcAServices.net>
2025-07-13 21:56:53 +0300

    Assign name to mongo container

--- tests/cypress-suite/bin/setup.sh
+++ tests/cypress-suite/bin/setup.sh
@@ -7,4 +7,4 @@ docker pull $1
 
 echo "Preparing Mongo container"
 docker network create development
-docker run -p "$MONGO_PORT:$MONGO_PORT" --network development -d mongo
+docker run --name mongo -p "$MONGO_PORT:$MONGO_PORT" --network development -d mongo
