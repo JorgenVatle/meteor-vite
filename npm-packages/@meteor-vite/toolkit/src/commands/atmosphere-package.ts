55cf0ef0166d580129b9f73a5962b7c8e7561d67
Jørgen Vatle <Jorgen@WcAServices.net>
2025-07-13 19:03:22 +0300

    Add shared parser fields to cli

--- npm-packages/@meteor-vite/toolkit/src/commands/atmosphere-package.ts
+++ npm-packages/@meteor-vite/toolkit/src/commands/atmosphere-package.ts
@@ -1,11 +1,19 @@
 import { CommandDefinition } from '@/lib/CommandDefinition';
+import { Parser } from '@/lib/CommandLineArgs/defineParser';
 import { CommandList } from '@/lib/CommandList';
+
+const sharedParser = new Parser({
+    rootDir: {
+        type: String,
+        description: `Path to the Meteor package's root directory. (where package.js lives)`
+    }
+})
 
 const subCommands = new CommandList([
     new CommandDefinition('version', {
         title: 'Update package.js version',
         description: `Patch Meteor's package.js file to match the current changeset release`,
-        fields: {},
+        fields: sharedParser.fields,
         handler: async () => {
         
         }
@@ -14,7 +22,7 @@ const subCommands = new CommandList([
     new CommandDefinition('publish', {
         title: 'Publish package to Atmosphere',
         description: `Build and publish the package to Atmosphere if out of date`,
-        fields: {},
+        fields: sharedParser.fields,
         handler: async () => {
         
         }
@@ -23,7 +31,7 @@ const subCommands = new CommandList([
     new CommandDefinition('format-changeset', {
         title: 'Format package name in changeset',
         description: 'Rewrites the package name in changesets and npm workspaces to ensure the package shows up in the correct format in the final changelog. (e.g. replaces jorgenvatle_vite with jorgenvatle:vite)',
-        fields: {},
+        fields: sharedParser.fields,
         handler: async () => {
         
         }
@@ -32,7 +40,7 @@ const subCommands = new CommandList([
     new CommandDefinition('unformat-changeset', {
         title: 'Revert changeset formatting',
         description: 'Revert changes from `format-changeset` to the original package name in the changeset. npm will complain otherwise',
-        fields: {},
+        fields: sharedParser.fields,
         handler: async () => {
         
         }
@@ -49,7 +57,7 @@ export default [
                 description: `Subcommand for Atmosphere package management.`,
                 defaultOption: true,
                 typeLabel: 'command',
-            }
+            },
         },
         subCommands,
         handler: async () => {
