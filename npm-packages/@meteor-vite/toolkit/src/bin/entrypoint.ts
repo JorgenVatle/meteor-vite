3b880e123efe7be95de2e88fd7f808ad43dd8138
Jørgen Vatle <Jorgen@WcAServices.net>
2025-07-13 16:48:04 +0300

    Remove unused consts

--- npm-packages/@meteor-vite/toolkit/src/bin/entrypoint.ts
+++ npm-packages/@meteor-vite/toolkit/src/bin/entrypoint.ts
@@ -1,8 +1,6 @@
 import { Commands } from '@/commands';
 import { CommandFailure } from '@/errors/CommandFailure';
 
-const [command, ...options] = process.argv.slice(2);
-
 await Commands.runWithParser().catch((error) => {
     if (!(error instanceof CommandFailure)) {
         throw error;
