160627fa76e460a0451cce155f7b801d80f606b2
Jørgen Vatle <Jorgen@WcAServices.net>
2025-07-13 21:55:56 +0300

    Start mongo sidecar container from setup script

--- tests/cypress-suite/bin/setup.sh
+++ tests/cypress-suite/bin/setup.sh
@@ -4,3 +4,7 @@ set -e
 
 echo "Pulling docker image: $1"
 docker pull $1
+
+echo "Preparing Mongo container"
+docker network create development
+docker run -p "$MONGO_PORT:$MONGO_PORT" --network development -d mongo
