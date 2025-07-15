c11c56232f7d1457b6aad1938632629620c2101f
Jørgen Vatle <Jorgen@WcAServices.net>
2025-07-15 03:54:26 +0300

    Use logger utility

--- examples/vue/server/main.js
+++ examples/vue/server/main.js
@@ -1,10 +1,12 @@
 // import("./../_vite-bundle/server/__entry.js").catch((e) => console.warn("Failed to import Meteor Server bundle from Vite! This may sometimes happen if it's your first time starting the app.", e));
+import { Logger } from './util';
+
 console.log('\n'.repeat(5));
 
 import('./vite-ssr.ts').then(async ({ init }) => {
     await init();
-    console.log('All should be good');
+    Logger.info('All should be good');
 }).catch((error) => {
-    console.error('\n\nWell so that didnt work\n\n', error);
+    Logger.error('\n\nWell so that didnt work\n\n', error);
     console.log('\n'.repeat(2));
 });
