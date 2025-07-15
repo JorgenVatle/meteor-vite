eb66b140824ba8911a7d3a7269ee83b41cefd569
Jørgen Vatle <Jorgen@WcAServices.net>
2025-07-15 04:21:51 +0300

    Run VM in new context

--- examples/vue/server/vite-ssr.ts
+++ examples/vue/server/vite-ssr.ts
@@ -1,10 +1,12 @@
 import { entryModules } from '/server/entryModules';
-import { Script } from 'vm';
+import vm from 'node:vm';
 
-const vm = new Script(entryModules.serverVm.sourceText, {
+const context = vm.createContext({});
+
+const script = new vm.Script(entryModules.serverVm.sourceText, {
     filename: '/home/jorgen/projects/meteor-vite/examples/vue/server/_vite-ssr.mts',
 });
 
-export const init = vm.runInThisContext({
+export const init = script.runInNewContext(context, {
     displayErrors: true,
 });
