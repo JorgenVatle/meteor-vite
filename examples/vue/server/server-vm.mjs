489fa4c13b41140033dee65cccd67d0c72c848af
Jørgen Vatle <Jorgen@WcAServices.net>
2025-07-15 15:52:41 +0300

    Use non-async imports

--- examples/vue/server/server-vm.mjs
+++ examples/vue/server/server-vm.mjs
@@ -1,15 +1,11 @@
-(async () => {
-    console.log('Importing Vite:');
-    const { createServer } = await import('vite');
+import { createServer } from 'vite';
 
+export async function start() {
     console.log('Initializing new Vite SSR Project...')
     const server = await createServer();
     console.log('Server created!');
     await server.listen();
     console.log('Server listening!');
     server.printUrls();
-
     console.log('Todo: Initialize SSR module runner');
-});
-
-// tes?
+}
