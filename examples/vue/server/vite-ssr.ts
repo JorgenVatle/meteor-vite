0edd88f8fa07321aa1d77d8bb4f9d2f050014361
Jørgen Vatle <Jorgen@WcAServices.net>
2025-07-15 04:58:37 +0300

    Add simulated source text module

--- examples/vue/server/vite-ssr.ts
+++ examples/vue/server/vite-ssr.ts
@@ -1,12 +1,41 @@
 import { entryModules } from '/server/entryModules';
+import { Logger } from '/server/util';
 import vm from 'node:vm';
 
 const context = vm.createContext({});
 
+function createLinker(): vm.ModuleLinker {
+    return (specifier, referrer, importAttributes) => {
+        Logger.info('Linking module', { specifier, referrer, importAttributes });
+        return Promise.resolve(simulated);
+    };
+}
+
+// language=javascript
+const simulated = new vm.SourceTextModule(`
+    export function createServer() {
+        console.log('Foo Bar!');
+    }
+`);
+
 const script = new vm.Script(entryModules.serverVm.sourceText, {
     filename: '/home/jorgen/projects/meteor-vite/examples/vue/server/_vite-ssr.mts',
+    importModuleDynamically: (specifier, referrer, importAttributes, phase) => {
+        Logger.info('Resolving dynamic import', {
+            module: specifier,
+            referrer,
+            importAttributes,
+            phase,
+        });
+        return Promise.resolve(simulated);
+    }
 });
 
-export const init = script.runInNewContext(context, {
-    displayErrors: true,
-});
+export async function init() {
+    const linker = createLinker();
+    await simulated.link(linker);
+    
+    return script.runInNewContext(context, {
+        displayErrors: true,
+    })();
+}
