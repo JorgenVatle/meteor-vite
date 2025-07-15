b49b40572a059ed5d2960c434aaf355596ec5b02
Jørgen Vatle <Jorgen@WcAServices.net>
2025-07-15 04:57:55 +0300

    Update log sections for main entry

--- examples/vue/server/main.js
+++ examples/vue/server/main.js
@@ -1,13 +1,21 @@
 // import("./../_vite-bundle/server/__entry.js").catch((e) => console.warn("Failed to import Meteor Server bundle from Vite! This may sometimes happen if it's your first time starting the app.", e));
 import { Logger } from './util';
+import pc from 'picocolors';
 
-console.log('\n'.repeat(5));
+console.log('\n'.repeat(3));
+
+Logger.info('ℹ️  Starting Meteor Vite Server\n');
 
 import('./vite-ssr.ts').then(async ({ init }) => {
     await init();
     Logger.info('All should be good');
 }).catch((error) => {
-    Logger.error('Well so that didnt work\n');
-    Logger.error(error);
-    setTimeout(() => console.log('\n'));
+    Logger.error('\n', error);
+    setTimeout(() => {
+        Logger.info([
+            '',
+            pc.bold(pc.whiteBright(`❌ Better luck next time!`)),
+            ''
+        ].join('\n'))
+    });
 });
