29a41af53cfe530d43b7b8b49d0cd63077bc6445
Jørgen Vatle <Jorgen@WcAServices.net>
2025-07-16 20:43:37 +0300

    Include dirname in cleanup messages

--- npm-packages/@meteor-vite/toolkit/src/ProjectCompiler.ts
+++ npm-packages/@meteor-vite/toolkit/src/ProjectCompiler.ts
@@ -188,14 +188,15 @@ export class ProjectCompiler {
     
     public async clean() {
         const path = Path.join(this.rootDir, 'dist');
+        
+        console.log(pc.bgYellow(pc.whiteBright(` Cleanup `)), pc.bold(this.rootDir));
         if (!await FS.lstat(path).catch(() => false)) {
-            console.log(pc.bgBlackBright(pc.whiteBright(' No dist directory found ')));
+            console.log(pc.bgBlackBright(pc.whiteBright(' No dist directory found')), '\n');
             return;
         }
-        console.log(pc.bgYellow(pc.whiteBright(' Cleaning dist directory ')));
         console.log(pc.gray(
             `Removing ${pc.yellow(pc.bold(path))}`
-        ))
+        ), '\n')
         await FS.rm(Path.join(this.rootDir, 'dist'), { recursive: true });
     }
     
