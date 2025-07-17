673008d7fe938747f3ff0312950429dbdae4c67b
Jørgen Vatle <Jorgen@WcAServices.net>
2025-07-17 04:55:15 +0300

    Assign base URL to vue-router instances

--- examples/vue-ssr/imports/ui/main.ts
+++ examples/vue-ssr/imports/ui/main.ts
@@ -1,15 +1,16 @@
-import { Meteor } from 'meteor/meteor'
+import { Meteor } from 'meteor/meteor';
 import { createSSRApp } from 'vue';
 import { createMemoryHistory, createRouter, createWebHistory } from 'vue-router';
 
-import App from './App.vue'
+import App from './App.vue';
 import { routes } from './router';
 
 export function createApp() {
-  let history = createMemoryHistory();
+  const base = Meteor.absoluteUrl('');
+  let history = createMemoryHistory(base);
   
   if (Meteor.isClient) {
-    history = createWebHistory();
+    history = createWebHistory(base);
   }
   
   const app = createSSRApp(App);
--- examples/vue/imports/ui/router.ts
+++ examples/vue/imports/ui/router.ts
@@ -1,8 +1,9 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from './Home.vue'
+import { Meteor } from 'meteor/meteor';
+import { createRouter, createWebHistory } from 'vue-router';
+import Home from './Home.vue';
 
 export const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(Meteor.absoluteUrl('')),
   routes: [
     {
       path: '/',
