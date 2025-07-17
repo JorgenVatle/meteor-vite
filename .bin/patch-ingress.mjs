91471f444931dbd32717ca661d8891e8f84b6a38
Jørgen Vatle <Jorgen@WcAServices.net>
2025-07-17 03:42:34 +0300

    Only patch ingress manifest if there are changes to make

--- .bin/patch-ingress.mjs
+++ .bin/patch-ingress.mjs
@@ -18,6 +18,14 @@ function getEnv(keys) {
     return Object.fromEntries(entries)
 }
 
+function patch(manifest) {
+    console.log(inspect(manifest.spec.rules, { colors: true, depth: 10 }));
+
+    execSync(`kubectl patch ingress preview -n ${APP_NAMESPACE} -p '${JSON.stringify(manifest)}'`, {
+        stdio: 'inherit'
+    })
+}
+
 const { APP_NAMESPACE, BASE_PATH, DEPLOYMENT_NAME } = getEnv(['APP_NAMESPACE', 'DEPLOYMENT_NAME', 'BASE_PATH']);
 
 try {
@@ -25,29 +33,36 @@ try {
         stdio: ['pipe', 'pipe', 'inherit']
     }).toString());
 
+    /**
+     * @type {{ paths: { path: string, pathType: string, backend: { service: { name: string, port: { number: number } } } }[] }}
+     * @const http
+     */
     const http = manifest.spec.rules[0].http
-    http.paths = http.paths.filter((rule) => {
-        return rule.path !== BASE_PATH;
-    });
 
-    http.paths.push({
-        path: BASE_PATH,
-        pathType: "Prefix",
-        backend: {
-            service: {
-                name: DEPLOYMENT_NAME,
-                port: {
-                    number: 3000
+    /**
+     * @type {{path: string, pathType: string, backend: {service: {name: string, port: {number: number}}}} | undefined}
+     */
+    let rule = http.paths.find(rule => rule.path === BASE_PATH);
+
+    if (!rule) {
+        http.paths.push({
+            path: BASE_PATH,
+            pathType: "Prefix",
+            backend: {
+                service: {
+                    name: DEPLOYMENT_NAME,
+                    port: {
+                        number: 3000
+                    }
                 }
-            }
-        },
-    });
+            },
+        });
+        patch(manifest)
+    } else if (rule.backend.service.name !== DEPLOYMENT_NAME) {
+        rule.backend.service.name = DEPLOYMENT_NAME;
+        patch(manifest)
+    }
 
-    console.log(inspect(manifest.spec.rules, { colors: true, depth: 10 }));
-
-    execSync(`kubectl patch ingress preview -n ${APP_NAMESPACE} -p '${JSON.stringify(manifest)}'`, {
-        stdio: 'inherit'
-    })
 } catch (error) {
     console.error(error.message);
     process.exitCode = 1;
