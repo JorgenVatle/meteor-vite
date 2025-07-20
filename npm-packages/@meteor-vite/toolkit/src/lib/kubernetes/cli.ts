4e519e93b6e191836bf131fb6e9d071683770bed
Jørgen Vatle <Jorgen@WcAServices.net>
2025-07-20 22:37:27 +0300

    Use class instead of helper function for kubectl

--- npm-packages/@meteor-vite/toolkit/src/lib/kubernetes/cli.ts
+++ npm-packages/@meteor-vite/toolkit/src/lib/kubernetes/cli.ts
@@ -4,21 +4,26 @@ import { execa } from 'execa';
 
 type Verb = 'get' | 'create' | 'delete' | 'apply';
 
-export function kubectl<
-    TType extends KubeResourceType,
-    TResource = KubeResource<TType>
->(verb: 'get', resource: TType, params: [name: string]): Promise<TResource>
-
-export function kubectl<
-    TType extends KubeResourceType,
-    TResource = KubeResource<TType>
->(verb: 'get', resource: TType, params: []): Promise<KubeResourceList<TResource>>
-
-export async function kubectl<
-    TType extends KubeResourceType,
-    TResource = KubeResource<TType>
->(verb: Verb, resource: TType, params: string[] = []): Promise<KubeResourceList<TResource> | TResource> {
-    const result = await execa('kubectl', [verb, resource, ...params, '-o', 'json']);
+class KubectlCli {
+    protected async kubectl<
+        TType extends KubeResourceType,
+        TResource = KubeResource<TType>
+    >(verb: Verb, ...params: string[]): Promise<KubeResourceList<TResource> | TResource> {
+        const result = await execa('kubectl', [verb, ...params, '-o', 'json']);
+        
+        return JSON.parse(result.stdout);
+    }
     
-    return JSON.parse(result.stdout);
+    public get<TType extends KubeResourceType>(resource: TType, options: { name?: string, params: string[] }): Promise<KubeResourceList<KubeResource<TType>> | KubeResource<TType>> {
+        const args: string[] = [...options.params];
+        if (options.name) {
+            args.unshift(options.name);
+        }
+        return this.kubectl('get', resource, ...args);
+    }
+    public apply(manifest: KubeResource): Promise<unknown> {
+        return this.kubectl('apply', '-f', '-', JSON.stringify(manifest));
+    }
 }
+
+export const kubectl = new KubectlCli();
