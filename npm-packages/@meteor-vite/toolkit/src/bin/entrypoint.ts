f60818660299bd6b7484d6dcf7f859d9d99b2788
Jørgen Vatle <Jorgen@WcAServices.net>
2025-07-20 17:35:16 +0300

    Remove misplaced debug logger from entrypoint

--- npm-packages/@meteor-vite/toolkit/src/bin/entrypoint.ts
+++ npm-packages/@meteor-vite/toolkit/src/bin/entrypoint.ts
@@ -1,8 +1,8 @@
 import { Commands } from '@/commands';
 import { CommandFailure } from '@/errors/CommandFailure';
+import 'source-map-support/register.js';
 
 await Commands.runWithParser().catch((error) => {
-    console.log(error);
     if (!(error instanceof CommandFailure)) {
         throw error;
     }
