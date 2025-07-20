58947e6b82d139aff6677260dc4681a443437e8d
Jørgen Vatle <Jorgen@WcAServices.net>
2025-07-20 22:25:38 +0300

    Add function overload types for get commands

--- npm-packages/@meteor-vite/toolkit/src/lib/kubernetes/cli.ts
+++ npm-packages/@meteor-vite/toolkit/src/lib/kubernetes/cli.ts
@@ -1,11 +1,23 @@
-import type { KubeManifest, KubeResourceList } from '@/lib/kubernetes/types/Generic';
+import type { KubeResourceList } from '@/lib/kubernetes/types/Generic';
+import type { KubeResource, KubeResourceType } from '@/lib/kubernetes/types/ResourceTypes';
 import { execa } from 'execa';
 
 type Verb = 'get' | 'create' | 'delete' | 'apply';
 
+export function kubectl<
+    TType extends KubeResourceType,
+    TResource = KubeResource<TType>
+>(verb: 'get', resource: TType, params: [name: string]): Promise<TResource>
+
+export function kubectl<
+    TType extends KubeResourceType,
+    TResource = KubeResource<TType>
+>(verb: 'get', resource: TType, params: []): Promise<KubeResourceList<TResource>>
+
 export async function kubectl<
-    T extends { kind: any } = KubeManifest | KubeResourceList
->(verb: Verb, resource: string, params: string[] = []): Promise<T> {
+    TType extends KubeResourceType,
+    TResource = KubeResource<TType>
+>(verb: Verb, resource: TType, params: string[] = []): Promise<KubeResourceList<TResource> | TResource> {
     const result = await execa('kubectl', [verb, resource, ...params, '-o', 'json']);
     
     return JSON.parse(result.stdout);
