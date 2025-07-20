96955853c640d37602a415476c9348916cb44fbd
Jørgen Vatle <Jorgen@WcAServices.net>
2025-07-20 22:20:03 +0300

    Add deployment manifest type

---
+++ npm-packages/@meteor-vite/toolkit/src/lib/kubernetes/types/DeploymentManifest.ts
@@ -0,0 +1,23 @@
+import type { KubeManifest } from '@/lib/kubernetes/types/Generic';
+
+export type DeploymentManifest = KubeManifest<{
+    selector: {
+        matchLabels: Record<string, string>;
+    }
+    template: PodTemplate
+}>;
+
+type PodTemplate = {
+    metadata: {
+        labels: Record<string, string>;
+        annotations?: Record<string, string>;
+    }
+    spec: {
+        containers: PodContainer[];
+    }
+}
+
+type PodContainer = {
+    name: string;
+    image: string;
+}
