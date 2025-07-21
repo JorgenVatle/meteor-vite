44988215f6c90d67cce333b153d41209be4fd4ab
Jørgen Vatle <Jorgen@WcAServices.net>
2025-07-21 11:57:13 +0300

    Print human duration for deployments still considered valid during prune

--- npm-packages/@meteor-vite/toolkit/src/commands/deploy-preview.ts
+++ npm-packages/@meteor-vite/toolkit/src/commands/deploy-preview.ts
@@ -313,7 +313,7 @@ export default [
                 const { timestamp, duration } = DeletionAnnotation.fromManifest(deployment);
                 
                 if (timestamp < Date.now()) {
-                    console.log(`Deployment ${deployment.metadata.name} is still valid until ${new Date(timestamp)}`);
+                    console.log(`Deployment ${deployment.metadata.name} is still valid until ${new Date(timestamp)} (${msToHumanDuration(timestamp - Date.now())})`);
                     continue;
                 }
              
@@ -323,6 +323,28 @@ export default [
         }
     })
 ];
+
+function msToHumanDuration(ms: number) {
+    const multipliers = {
+        days: 1000 * 60 * 60 * 24,
+        hours: 1000 * 60 * 60,
+        minutes: 1000 * 60,
+    }
+    
+    if (ms > multipliers.days) {
+        return `${Math.round(ms / multipliers.days)} days`;
+    }
+    
+    if (ms > multipliers.hours) {
+        return `${Math.round(ms / multipliers.hours)} hours`;
+    }
+    
+    if (ms > multipliers.minutes) {
+        return `${Math.round(ms / multipliers.minutes)} minutes`;
+    }
+    
+    return `${Math.round(ms / 1000)} seconds`;
+}
 
 class DeletionAnnotation {
     public readonly timestamp: number;
