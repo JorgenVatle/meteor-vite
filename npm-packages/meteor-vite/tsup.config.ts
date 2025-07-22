9f044dbeff13321ef9447be40ade2b1dc786edf4
Jørgen Vatle <Jorgen@WcAServices.net>
2025-07-22 20:25:10 +0300

    Revert "Rename internals index to main.ts to avoid issues with path aliases during d.ts generation."
    This reverts commit 8fd110c5dfed7a530d9546d25b2f76bbcbfb2bd7.

--- npm-packages/meteor-vite/tsup.config.ts
+++ npm-packages/meteor-vite/tsup.config.ts
@@ -11,7 +11,7 @@ export default defineBuildConfig(__dirname, [
             'plugin': './src/plugin/index.ts',
             
             // Internal tooling for the Meteor build plugin.
-            'internals': './src/internals/main.ts',
+            'internals': './src/internals/index.ts',
         },
         format: ['esm'],
         platform: 'node',
