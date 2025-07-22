77afcea9735e50d5b3a1d5bdd361f9769482fed4
Jørgen Vatle <Jorgen@WcAServices.net>
2025-07-22 21:16:56 +0300

    Add missing name to build config

--- npm-packages/@meteor-vite/plugin-zodern-relay/tsup.config.ts
+++ npm-packages/@meteor-vite/plugin-zodern-relay/tsup.config.ts
@@ -1,5 +1,6 @@
 import { defineBuildConfig } from '@meteor-vite/toolkit';
 
 export default defineBuildConfig(import.meta.dirname, {
+    name: '@meteor-vite/plugin-zodern-relay',
     entry: ['src/Plugin.ts'],
 })
